baf77c7008bd034ddeb97dd40a91c8f00a13f10c
Marcathian Alexander <marcathian@gmail.com>
2012-02-02 15:32:34 -0400

    Added setup.py and third_file.ext

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="Marcathian_Geonode_Training1"
+    version="0.1",
+    author="Marcathian Alexander",
+    author_email="marcathian@gmail.com",
+    description="Sample project",
+    long_description=(read('README')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: GeoNode',
+        'License :: OSI Approved :: BSD',
+    ],
+    license="BSD",
+    keywords="geonode django",
+    url='https://github.com/marcathian/Marcathian_Geonode_Training1',
+    scripts = [
+               'scripts/Marcathian_Geonode_Training1',
+              ],
+    packages=find_packages('.'),
+    include_package_data=True,
+    zip_safe=False,
+)
